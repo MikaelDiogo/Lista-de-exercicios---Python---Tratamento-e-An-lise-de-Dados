c7e41e712724380919aa22f1197942c20d1a59e6
MikaelDiogo <642mikaeldiogo@gmail.com>
2026-01-30 19:14:18 -0300

    add q6

--- Q06.py
+++ Q06.py
@@ -4,4 +4,3 @@ total_cigarros = cigarros_dia * 365 * anos
 minutos = total_cigarros * 10
 dias_perdidos = minutos / 1440
 print("A quantidade de dias de vida perdidos é:", dias_perdidos)
-print("gerado por ia")
